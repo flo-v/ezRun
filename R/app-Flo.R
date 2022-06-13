ad010ee9f818e0349fb12e32005605dbd8303db0
Florian Vetsch <florian.vetsch@uzh.ch>
2022-06-13 15:55:05 +0200

    Revert "Update app-Flo.R"
    This reverts commit 982c07dfcbf53ed0d16b488f32522faf8953e36a.

--- R/app-Flo.R
+++ R/app-Flo.R
@@ -14,33 +14,9 @@ ezMethodFlo <- function(input = NA, output = NA, param = NA,
   ans4Report[["dataset"]] <- dataset
 
   output_dir <- basename(output$getColumn("Report"))
-  prefix <- file.path(output_dir, "vcf_stats")
-
-  # For Rmd
-  # SNP counts
-  snp_counts <- file.path(output_dir, "vcf_stats.snps")
-
-  # InDel counts
-  # ToDo
-
-  # Private SNP counts
-  private_snp_counts <- file.path(output_dir, "vcf_stats.private")
-
-  # Shared SNP counts
-  shared_snp_counts <- file.path(output_dir, "vcf_stats.shared")
-
-  # Transions/Transversions
-  tstv <- file.path(output_dir, "vcf_stats.samples-tstv")
-
-  # run vcf-stats
-  cmd <- paste("vcf-stats", file.path("/srv/gstore/projects", input$getColumn("Filtered VCF")), "-p", prefix)
-  result <- ezSystem(cmd)
-  gc()
-
   
   # convert vcf to gds format
   library(SNPRelate)
-  # library(SeqArray) # not installed on server
   library("gdsfmt")
   vcf.fn <- file.path("/srv/gstore/projects", input$getColumn("Filtered VCF"))
   # Reformat
